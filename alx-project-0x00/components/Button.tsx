ab959df7acbfcb4cb8555b45ada23816ca2f9f3c
Mokete <ematyabekana@gmail.com>
2025-07-15 20:45:44 +0200

    Created a button component wwith different size instances

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Button: React.FC = () => {
+  return (
+    <button className="border px-3 py-1 text-lg mt-3 bg-amber-500 text-white sm:size-sm md:size-md lg:size-lg sm:rounded-sm md:rounded-md lg:rounded-full">
+      Submit
+    </button>
+  )
+}
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,13 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button";
+import React from "react";
 
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
      <Card />
+     <Button/>
     </div>
   )
 }
